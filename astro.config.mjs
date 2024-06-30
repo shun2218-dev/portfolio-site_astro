0b8a9710dcddf012bf9d03134105b99cdcb49fdd
shun2218-dev <kohaku1015game@gmail.com>
2024-07-01 01:01:24 +0900

    refactor: 💡 remove vue from integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import { defineConfig } from 'astro/config';
-
-import vue from "@astrojs/vue";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [],
 });
